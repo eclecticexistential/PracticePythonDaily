646a9fef54b82856bdb40f2a90f0f56ca0d1a14a
eclecticexistential <allofitbra@gmail.com>
2017-12-03 10:41:20 -0800

    Moved shopping list app to its own file. Placed everything inside of its own function for scalability purposes.

---
+++ shopping_list.py
@@ -0,0 +1,56 @@
+
+def start_shopping():
+    print("Add items or say 'show' to see your list, 'help' to view options, or 'done' to exit and view list.")
+
+start_shopping()
+
+def help():
+    start_shopping()
+
+def show(x):
+    print(','.join(x))
+
+def main():
+    new_item = ""
+    shopping_list = list()
+    list_output = "You need to buy: "
+    solo_list = "You only need to pick up: "
+
+    while new_item != 'done':
+
+        new_item = input("What do you need at the store?").lower()
+
+        if len(shopping_list) == 0 and new_item == "done":
+            print('Goodbye. Have a nice day!')
+
+        elif new_item == "help":
+            help()
+
+        elif new_item == 'show':
+            show(shopping_list)
+
+        elif new_item == 'done':
+                list_len = len(shopping_list)
+                for i in range(0, list_len):
+                    if list_len == 1:
+                        solo_list += shopping_list[i]
+                        solo_list += "."
+                        print(solo_list)
+                    elif i < len(shopping_list)-1:
+                        list_output += shopping_list[i]
+                        if list_len == 2:
+                            list_output += " and "
+                        elif list_len > 2:
+                            list_output += ", "
+                    else:
+                        if list_len > 2:
+                            list_output += "and "
+
+                        list_output += shopping_list[i]
+                        list_output += "."
+                        print(list_output)
+        else:
+            shopping_list.append(new_item)
+            print("Added {}. List now has {} items.".format(new_item, len(shopping_list)))
+
+main()
