5ceeec6f1ae8f3aacadcf3e4fd22ce2654d16414
eclecticexistential <allofitbra@gmail.com>
2017-12-15 12:19:33 -0800

    Seperated virt id and account id into respective components. Used this structure to introduce fail safes into account creation. Adjusted output to be a bit more user friendly.

--- bankingClass.py
+++ bankingClass.py
@@ -1,42 +1,80 @@
 import random
 
-bank_accounts = []
+bank_accounts = {}
 num_words = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m',0,1,2,3,4,5,6,7,8,9,'n','o','p','q','r','s','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9]
 num_only = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9]
 
-def get_account():
+def get_virtual_ID():
     iter = 11
     accountComp = ""
-    accountHuman = ""
-    accountDict = dict()
     while iter > 0:
         digitComp = random.choice(num_words)
-        digitHuman = random.choice(num_only)
         accountComp += str(digitComp)
-        accountHuman += str(digitHuman)
         iter -= 1
 
-    accountDict[0] = accountComp
-    accountDict[1] = accountHuman
-    return accountDict
+    return accountComp
+
+def get_account():
+    iter = 11
+    accountHuman = ""
+    while iter > 0:
+        digitHuman = random.choice(num_only)
+        accountHuman += str(digitHuman)
+        iter -= 1
+    return accountHuman
+
+def create_account(full_name,social,address):
+    virtID = get_virtual_ID()
+    account_num = get_account()
+    issues = 0
+    account_info = dict()
+    for i in bank_accounts:
+        if i == virtID:
+            while i == virtID:
+                try:
+                    virtID = get_virtual_ID()
+                finally:
+                    if i == virtID:
+                        print("Unable to create new Virtual ID. Contact admin.")
+                        issues += 1
+                        break
+        if bank_accounts[i][0] == account_num:
+            while bank_accounts[i][0] == account_num:
+                try:
+                    account_num = get_account()
+                finally:
+                    if bank_accounts[i][0] == account_num:
+                        print("Unable to create new Account Number. Contact Admin.")
+                        issues += 1
+                        break
+    if issues == 0:
+        account_info[0] = account_num
+        account_info[1] = full_name
+        account_info[2] = social
+        account_info[3] = address
+        bank_accounts[virtID]=account_info
+        return bank_accounts
 
 class Person(object):
-    def __init__(self):
-        self.first_name = input("First Name? ")
-        self.last_name = input("Last Name? ")
-    def account_number(self):
-        self.account_number = get_account()
-        return self.account_number
-    def add_account(self):
-        self.info = [self.first_name,self.last_name,self.account_number]
-        bank_accounts.append(self.info)
+    def __init__(self,first_name,last_name,social,address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.social = social
+        self.address = address
+    def get_account_number(self):
+        self.full_name = self.first_name, self.last_name
+        self.account_info = create_account(self.full_name, self.social, self.address)
+        return self.account_info
     def get_info(self):
-        x.account_number()
-        x.add_account()
-        return "Virtual ID: {} \nAccount Number: {} \nName: {} {}".format(bank_accounts[0][2][0],
-                                                             bank_accounts[0][2][1],bank_accounts[0][0], bank_accounts[0][1])
+        self.get_account_number()
 
 
-x = Person()
+x = Person("Jared","Smith",555009999,"240 W. Main St")
+y = Person("Rebecca","Codwell",111223333,"555 E. Market Ave")
+z = Person("Harley","Jones",131720369,"500 N. Street Ave")
+x.get_info()
+y.get_info()
+z.get_info()
 
-print(x.get_info())
+for i in bank_accounts:
+    print("Virtual ID: {} \nAccount Number: {} \nName: {} \nSocial: {} \nAddress: {}\n".format(i,bank_accounts[i][0],' '.join(bank_accounts[i][1]),bank_accounts[i][2],bank_accounts[i][3]))
