16060e7eec074ce00366f96caeb9c302176e1924
eclecticexistential <allofitbra@gmail.com>
2018-01-13 02:03:49 -0800

    Added programming for tri-color decks in limited.

--- mtg_evo_wilds_stats.py
+++ mtg_evo_wilds_stats.py
@@ -1,5 +1,179 @@
 import random
 
+# #two mana deck
+#
+# def open_hand(x):
+#     counter = 7
+#     hand = []
+#     lands_on_field = []
+#     while counter > 0:
+#         a = random.choice(x)
+#         x.remove(a)
+#         hand.append(a)
+#         counter -= 1
+#     data = [hand, x, lands_on_field]
+#     return data
+#
+# def remove_card_from_deck(deck,hand):
+#     a = random.choice(deck)
+#     deck.remove(a)
+#     hand.append(a)
+#     return [deck,hand]
+#
+# def hands(x):
+#     random.shuffle(x)
+#     # mixes up deck
+#     hand, deck, lands_on_field = open_hand(x)
+#     #gets opening hand of 7 cards, removes them from deck, creates field variable: an array
+#     return hand,deck,lands_on_field
+#
+# def hand_check(hand):
+#     #discards a 1 to take into account creatures/spells that are played during each round
+#     if len(hand) > 7:
+#         for cards in hand:
+#             if cards == 1:
+#                 hand.remove(cards)
+#                 break
+#
+# def play_land(deck,hand,field):
+#     for card in hand:
+#         #takes care of evolving wilds
+#         if card == 10:
+#             hand.remove(card)
+#             color2 = 0
+#             color3 = 0
+#             #checks to see which land is needed
+#             for lands in field:
+#                 if lands == 2:
+#                     color2+= 1
+#                 elif lands == 3:
+#                     color3+= 1
+#             for cards in deck:
+#                 #puts basic on the field
+#                 if color2 > color3:
+#                     if cards == 3:
+#                         field.append(cards)
+#                         deck.remove(cards)
+#                         random.shuffle(deck)
+#                         return
+#                 elif color3 > color2:
+#                     if cards == 2:
+#                         field.append(cards)
+#                         deck.remove(cards)
+#                         random.shuffle(deck)
+#                         return
+#                 elif cards == 2 or cards == 3:
+#                     if color2 >=4 and color3 >=4:
+#                         field.append(cards)
+#                         deck.remove(cards)
+#                         random.shuffle(deck)
+#                         return
+#                     elif color2 == color3:
+#                         field.append(cards)
+#                         deck.remove(cards)
+#                         random.shuffle(deck)
+#                         return
+#         #plays land otherwise
+#         elif card == 2 or card == 3:
+#             field.append(card)
+#             hand.remove(card)
+#             return
+#
+# def check_field(hand,field):
+#     num2s = 0
+#     num3s = 0
+#     #counts the lands to see if an adequate amount of land is available to cast 'win condition' cards: 42
+#     for card in field:
+#         if card == 2:
+#             num2s += 1
+#         elif card == 3:
+#             num3s += 1
+#     #8 mana total to cast "win condition" cards
+#     if num2s >= 4 and num3s >= 4:
+#         for cards in hand:
+#             if cards == 42:
+#                 return "Winner!"
+#
+#
+# def turn(hand, deck=None,field=None):
+#     counter = 0
+#     #counts number of draws
+#     wins = 0
+#     #when this is 1 game ends
+#     stuck_mana1 = 0
+#     stuck_mana2 = 0
+#     stuck_mana3 = 0
+#     #tallies when player is stuck with either 1,2 or 3 mana for several rounds...commonly leading to a loss IRL
+#     while len(deck) > 0:
+#         remove_card_from_deck(deck,hand)
+#         play_land(deck,hand,field)
+#         hand_check(hand)
+#         game_over = check_field(hand,field)
+#         if game_over == "Winner!":
+#             wins += 1
+#             break
+#         #takes into account getting mana starved
+#         elif game_over != "Winner!":
+#             if len(field) == 1:
+#                 stuck_mana1 += 1
+#             elif len(field) == 2:
+#                 stuck_mana2 += 1
+#             elif len(field) == 3:
+#                 stuck_mana3 += 1
+#         if stuck_mana1 > 1 or stuck_mana2 > 3 or stuck_mana3 >3:
+#             wins = -1
+#             break
+#         counter += 1
+#     if wins == 1:
+#         return counter
+#     elif wins == -1:
+#         return 0
+#
+# def status():
+#     non_evo_wilds = [42, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+#                      1, 1, 1, 1, 1, 1, 42]
+#     #has two win condition cards
+#     evo_wilds = [42, 10, 10, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+#                  1, 1, 1, 1, 1, 42]
+#     #has two win condition cards, two evolving wilds, 7 of one color, 8 of another, and 21 creature/spells
+#     p1_hand, p1_deck, p1_field = hands(evo_wilds)
+#     p2_hand, p2_deck, p2_field = hands(non_evo_wilds)
+#     #taking turns
+#     totals_for_p1 = turn(p1_hand, deck=p1_deck,field=p1_field)
+#     totals_for_p2 = turn(p2_hand,deck=p2_deck,field=p2_field)
+#     stats = [totals_for_p1,totals_for_p2]
+#     return stats
+#
+# def out_of_all_games():
+#     evo_wilds_wins = 0
+#     non_evo_wins = 0
+#     evo_draw_steps = []
+#     non_evo_wins_steps = []
+#     games = 100000
+#     while games > 0:
+#         p1_draws_into_win, p2_draws_into_win = status()
+#
+#         evo_draw_steps.append(p1_draws_into_win)
+#         non_evo_wins_steps.append(p2_draws_into_win)
+#
+#         if p1_draws_into_win > p2_draws_into_win:
+#             evo_wilds_wins += 1
+#         elif p1_draws_into_win < p2_draws_into_win:
+#             non_evo_wins += 1
+#         elif p1_draws_into_win == p2_draws_into_win:
+#             evo_wilds_wins += 1
+#             non_evo_wins += 1
+#         games -= 1
+#     print("Evo Deck Wins {}".format(evo_wilds_wins))
+#     print("Non Evo Deck Wins {}".format(non_evo_wins))
+#     print("With Evolving Wilds: {} Cards Drawn Into Win Condition.".format(sum(evo_draw_steps)/len(evo_draw_steps)))
+#     print("Without Evolving Wilds: {} Cards Drawn Into Win Condition.".format(sum(non_evo_wins_steps)/len(non_evo_wins_steps)))
+#
+#
+# out_of_all_games()
+
+#three mana deck
+
 def open_hand(x):
     counter = 7
     hand = []
@@ -40,12 +214,15 @@ def play_land(deck,hand,field):
             hand.remove(card)
             color2 = 0
             color3 = 0
+            color4 = 0
             #checks to see which land is needed
             for lands in field:
                 if lands == 2:
                     color2+= 1
                 elif lands == 3:
                     color3+= 1
+                elif lands == 4:
+                    color4+= 1
             for cards in deck:
                 #puts basic on the field
                 if color2 > color3:
@@ -54,20 +231,43 @@ def play_land(deck,hand,field):
                         deck.remove(cards)
                         random.shuffle(deck)
                         return
-                elif color3 > color2:
+                if color2 > color4:
+                    if cards == 4:
+                        field.append(cards)
+                        deck.remove(cards)
+                        random.shuffle(deck)
+                        return
+                if color3 > color2:
                     if cards == 2:
                         field.append(cards)
                         deck.remove(cards)
                         random.shuffle(deck)
                         return
-                elif cards == 2 or cards == 3:
-                    if color2 >=4 and color3 >=4:
+                if color3 > color4:
+                    if cards == 4:
+                        field.append(cards)
+                        deck.remove(cards)
+                        random.shuffle(deck)
+                        return
+                if color4 > color3:
+                    if cards == 3:
+                        field.append(cards)
+                        deck.remove(cards)
+                        random.shuffle(deck)
+                        return
+                if color4 > color2:
+                    if cards == 2:
+                        field.append(cards)
+                        deck.remove(cards)
+                        random.shuffle(deck)
+                        return
+                elif cards == 2 or cards == 3 or cards == 4:
                         field.append(cards)
                         deck.remove(cards)
                         random.shuffle(deck)
                         return
         #plays land otherwise
-        elif card == 2 or card == 3:
+        elif card == 2 or card == 3 or card == 4:
             field.append(card)
             hand.remove(card)
             return
@@ -75,14 +275,17 @@ def play_land(deck,hand,field):
 def check_field(hand,field):
     num2s = 0
     num3s = 0
+    num4s = 0
     #counts the lands to see if an adequate amount of land is available to cast 'win condition' cards: 42
     for card in field:
         if card == 2:
             num2s += 1
         elif card == 3:
             num3s += 1
-    #8 mana total to cast "win condition" cards
-    if num2s >= 4 and num3s >= 4:
+        elif card == 4:
+            num4s += 1
+    # mana total to cast "win condition" cards
+    if num2s >= 2 and num3s >= 2 and num4s >= 2:
         for cards in hand:
             if cards == 42:
                 return "Winner!"
@@ -114,16 +317,20 @@ def turn(hand, deck=None,field=None):
             elif len(field) == 3:
                 stuck_mana3 += 1
         if stuck_mana1 > 1 or stuck_mana2 > 3 or stuck_mana3 >3:
+            wins = -1
             break
         counter += 1
-    return counter
+    if wins == 1:
+        return counter
+    elif wins == -1:
+        return 0
 
 def status():
-    non_evo_wilds = [42, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    non_evo_wilds = [42, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                      1, 1, 1, 1, 1, 1, 42]
     #has two win condition cards
-    evo_wilds = [42, 10, 10, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                 1, 1, 1, 1, 1, 42]
+    evo_wilds = [42, 10, 10, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                 1, 1, 1, 1, 1, 1, 42]
     #has two win condition cards, two evolving wilds, 7 of one color, 8 of another, and 21 creature/spells
     p1_hand, p1_deck, p1_field = hands(evo_wilds)
     p2_hand, p2_deck, p2_field = hands(non_evo_wilds)
@@ -150,14 +357,20 @@ def out_of_all_games():
             evo_wilds_wins += 1
         elif p1_draws_into_win < p2_draws_into_win:
             non_evo_wins += 1
-        else:
+        elif p1_draws_into_win == p2_draws_into_win:
             ties += 1
         games -= 1
     print("Evo Deck Wins {}".format(evo_wilds_wins))
     print("Non Evo Deck Wins {}".format(non_evo_wins))
-    print("Ties {}\n".format(ties))
     print("With Evolving Wilds: {} Cards Drawn Into Win Condition.".format(sum(evo_draw_steps)/len(evo_draw_steps)))
     print("Without Evolving Wilds: {} Cards Drawn Into Win Condition.".format(sum(non_evo_wins_steps)/len(non_evo_wins_steps)))
 
 
 out_of_all_games()
+
+#display creatures ... 4 on field == win condition
+#have card to remove 42 from deck
+#two or more creatures on one field vs the other
+
+#magic api starcity,tcg for data sets
+#tcg life duration price of card
